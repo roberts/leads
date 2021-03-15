e831b263794b3e30a5ed2e7037b30171f4094185
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-15 19:25:49 -0300

    Register all components

--- src/Enums/OnboardingFormStep.php
+++ src/Enums/OnboardingFormStep.php
@@ -11,9 +11,9 @@ final class OnboardingFormStep extends Enum
     const MAILING_ADDRESS = 'mailing-address';
     const BUSINESS_DETAILS = 'business-details';
     const PAYROLL_CLASSIFICATIONS = 'payroll-classifications';
-    const COMP_INSURANCE = 'comp-insurance';
     const COMP_INSURANCE_CHECK = 'comp-insurance-check';
-    const COMP_CLAIMS = 'comp-claims';
+    const COMP_INSURANCE = 'comp-insurance';
     const COMP_CLAIMS_CHECK = 'comp-claims-check';
+    const COMP_CLAIMS = 'comp-claims';
     const COMPLETED = 'completed';
 }
--- src/WorkCompLeadsServiceProvider.php
+++ src/WorkCompLeadsServiceProvider.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Roberts\WorkCompLeads;
 
 use Livewire\Livewire;
@@ -34,5 +32,15 @@ class WorkCompLeadsServiceProvider extends TipoffServiceProvider
     protected function registerLivewireComponents()
     {
         Livewire::component('wc::onboarding-form', OnboardingForm::class);
+        Livewire::component('wc::presentation', OnboardingForm\Presentation::class);
+        Livewire::component('wc::contact-details', OnboardingForm\ContactDetails::class);
+        Livewire::component('wc::mailing-address', OnboardingForm\MailingAddress::class);
+        Livewire::component('wc::business-details', OnboardingForm\BusinessDetails::class);
+        Livewire::component('wc::payroll-classifications', OnboardingForm\PayrollClassifications::class);
+        Livewire::component('wc::comp-insurance-check', OnboardingForm\CompInsuranceCheck::class);
+        Livewire::component('wc::comp-insurance', OnboardingForm\CompInsurance::class);
+        Livewire::component('wc::comp-claims-check', OnboardingForm\CompClaimsCheck::class);
+        Livewire::component('wc::comp-claims', OnboardingForm\CompClaims::class);
+        Livewire::component('wc::completed', OnboardingForm\Completed::class);
     }
 }
