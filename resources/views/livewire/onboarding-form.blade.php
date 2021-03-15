31ac4a503c2e15b60a00a4cb55b99d98caf19001
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-15 19:16:34 -0300

    Fix component loading inside view

--- resources/views/livewire/onboarding-form.blade.php
+++ resources/views/livewire/onboarding-form.blade.php
@@ -1,23 +1,23 @@
 <div class="onboarding-form">
     @if ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::PRESENTATION)
-        <livewire:onboarding-form.presentation :lead="$lead" />
+        @livewire('onboarding-form.presentation', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::CONTACT_DETAILS)
-        <livewire:onboarding-form.contact-details :lead="$lead" />
+        @livewire('onboarding-form.contact-details', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::MAILING_ADDRESS)
-        <livewire:onboarding-form.mailing-address :lead="$lead" />
+        @livewire('onboarding-form.mailing-address', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::BUSINESS_DETAILS)
-        <livewire:onboarding-form.business-details :lead="$lead" />
+        @livewire('onboarding-form.business-details', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::PAYROLL_CLASSIFICATIONS)
-        <livewire:onboarding-form.payroll-classifications :lead="$lead" />
+        @livewire('onboarding-form.payroll-classifications', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::COMP_INSURANCE_CHECK)
-        <livewire:onboarding-form.comp-insurance-check :lead="$lead" />
+        @livewire('onboarding-form.comp-insurance-check', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::COMP_INSURANCE)
-        <livewire:onboarding-form.comp-insurance :lead="$lead" />
+        @livewire('onboarding-form.comp-insurance', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::COMP_CLAIMS_CHECK)
-        <livewire:onboarding-form.comp-claims-check :lead="$lead" />
+        @livewire('onboarding-form.comp-claims-check', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::COMP_CLAIMS)
-        <livewire:onboarding-form.comp-claims :lead="$lead" />
+        @livewire('onboarding-form.comp-claims', ['lead' => $lead])
     @elseif ($step === \Roberts\WorkCompLeads\Enums\OnboardingFormStep::COMPLETED)
-        <livewire:onboarding-form.completed :lead="$lead" />
+        @livewire('onboarding-form.completed', ['lead' => $lead])
     @endif
 </div>
