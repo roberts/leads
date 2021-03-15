bdad5f5e7642769ac0ed98864fbb7f685f9ba2e7
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-15 16:44:42 -0300

    Create abstract component to handle lead processing

---
+++ Livewire/OnboardingForm/OnboardingFormStepComponent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire\OnboardingForm;
+
+use Livewire\Component;
+
+abstract class OnboardingFormStepComponent extends Component
+{
+    public $lead;
+    public $attributes;
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+        $data = $this->validate();
+
+        $this->lead = $this->processLead($data['attributes']);
+
+        $this->emit('completed', $this->getNextStep());
+        $this->emit('lead-updated', $this->lead->id);
+    }
+
+    abstract public function processLead(array $data);
+
+    abstract public function getNextStep();
+}
