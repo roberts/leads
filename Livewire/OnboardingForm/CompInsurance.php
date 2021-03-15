df8e4d8232da1c7c50b308d6e20384a47d92993d
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-15 16:52:05 -0300

    Add comp insurance step

---
+++ Livewire/OnboardingForm/CompInsurance.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire\OnboardingForm;
+
+use App\Enums\OnboardingFormStep;
+
+class CompInsurance extends OnboardingFormStepComponent
+{
+    protected $rules = [
+        'attributes.current_plan_under_cancellation' => 'required|boolean',
+        'attributes.current_plan_expires_at' => 'required',
+    ];
+
+    public function render()
+    {
+        return view('livewire.onboarding-form.comp-insurance');
+    }
+
+    public function processLead(array $data)
+    {
+        return $this->lead;
+    }
+
+    public function getNextStep()
+    {
+        return OnboardingFormStep::COMP_CLAIMS_CHECK;
+    }
+}
