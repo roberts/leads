3e0d21f0d7d575821a4cfbdbe32dc07b6785de48
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-15 16:42:15 -0300

    Create enum for the form steps

---
+++ Enums/OnboardingFormStep.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class OnboardingFormStep extends Enum
+{
+    const PRESENTATION = 'presentation';
+    const CONTACT_DETAILS = 'contact-details';
+    const MAILING_ADDRESS = 'mailing-address';
+    const BUSINESS_DETAILS = 'business-details';
+    const PAYROLL_CLASSIFICATIONS = 'payroll-classifications';
+    const COMP_INSURANCE = 'comp-insurance';
+    const COMP_INSURANCE_CHECK = 'comp-insurance-check';
+    const COMP_CLAIMS = 'comp-claims';
+    const COMP_CLAIMS_CHECK = 'comp-claims-check';
+    const COMPLETED = 'completed';
+}
