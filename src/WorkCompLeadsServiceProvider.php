80393ef17b51bd734d12d24bb48f360602468468
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-15 17:26:03 -0300

    Register onboarding-form component

--- src/WorkCompLeadsServiceProvider.php
+++ src/WorkCompLeadsServiceProvider.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace Roberts\WorkCompLeads;
 
+use Livewire\Livewire;
+use Roberts\WorkCompLeads\Livewire\OnboardingForm;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
 class WorkCompLeadsServiceProvider extends TipoffServiceProvider
 {
+    public function boot()
+    {
+        parent::boot();
+
+        Livewire::component('onboarding-form', OnboardingForm::class);
+    }
+
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
