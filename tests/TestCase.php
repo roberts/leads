45082195800463140bf4352130f754fe1fa2f59e
Hunter Montell <64396917+huntermontell@users.noreply.github.com>
2021-03-21 16:08:23 -0500

    Remove import of deleted class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,12 +7,12 @@ namespace Roberts\Leads\Tests;
 use Laravel\Nova\NovaCoreServiceProvider;
 use Livewire\LivewireServiceProvider;
 use Roberts\Leads\LeadsServiceProvider;
-use Roberts\Leads\Tests\Support\Providers\NovaTestbenchServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Statuses\StatusesServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
+use Tipoff\TestSupport\Providers\NovaPackageServiceProvider;
 
 class TestCase extends BaseTestCase
 {
