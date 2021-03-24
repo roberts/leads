2c941f1dada294ae3e6f35238b59f9261c1acacc
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-24 20:15:14 -0300

    Point css to published vendor file

--- resources/views/onboarding-form.blade.php
+++ resources/views/onboarding-form.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/leads/css/app.css') }}" rel="stylesheet">
 
     <livewire:styles />
 </head>
