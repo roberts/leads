afb6604f7174a3ee10d81ac5f5549e1cb5017d5f
Gustavo Gentil <gustavorgentil@outlook.com>
2021-03-31 01:30:48 -0300

    Add textarea blade component

---
+++ resources/views/inputs/textarea.blade.php
@@ -0,0 +1,16 @@
+<div class="onboarding-form__block-group">
+    <textarea
+        wire:model="attributes.{{ $field->name }}"
+        class="onboarding-form__text-input @if(!empty($attributes[$field->name])) onboarding-form__text-input--filled @endif"
+        id="{{ $field->name }}"
+    ></textarea>
+
+    @error('attributes.' . $field->name) <p class="onboarding-form__error">{{ $message }}</p> @enderror
+
+    <label
+        for="{{ $field->name }}"
+        class="onboarding-form__label"
+    >
+        {{ $field->label }}
+    </label>
+</div>
