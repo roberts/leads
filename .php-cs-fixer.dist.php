6a20e67c831f3c6d2d0b85e66d452aa9ae8ff336
Drew Roberts <github@drewroberts.com>
2022-08-12 11:49:25 -0400

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -19,7 +19,7 @@ $config
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
         'trailing_comma_in_multiline' => true,
