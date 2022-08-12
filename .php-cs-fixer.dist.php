46772c03a74aa060d6c15c3688e23343fd5d48b1
Drew Roberts <github@drewroberts.com>
2022-08-12 11:55:10 -0400

    Remove class attributes separation method

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -31,11 +31,6 @@ $config
         ],
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
-        'class_attributes_separation' => [
-            'elements' => [
-                'method',
-            ],
-        ],
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
